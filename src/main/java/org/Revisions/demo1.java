619ea63a8376ec9f2d28c82cacafa7dcd96a75c0
Amit Kumar <akumar420india@gmail.com>
2025-04-11 17:34:00 +0530

    abc

--- src/main/java/org/Revisions/demo1.java
+++ src/main/java/org/Revisions/demo1.java
@@ -3,5 +3,8 @@ package org.Revisions;
 public class demo1 {
     public static void main(String[] args) {
         System.out.println("Hello From PSA");
+        System.out.println("Hello From Amit1");
+        System.out.println("Hello From Amit2");
+        System.out.println("Hello From Amit3");
     }
 }
