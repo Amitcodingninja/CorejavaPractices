a54a62b31b2b76bbeb99245e1e8d0330cc4ac4e2
Amit Kumar <akumar420india@gmail.com>
2025-04-11 17:25:26 +0530

    new change

--- src/main/java/org/Revisions/demo1.java
+++ src/main/java/org/Revisions/demo1.java
@@ -3,5 +3,10 @@ package org.Revisions;
 public class demo1 {
     public static void main(String[] args) {
         System.out.println("Hello From PSA");
+
+
+        System.out.println("New Changes1");
+        System.out.println("New Changes2");
+        System.out.println("New Changes3");
     }
 }
